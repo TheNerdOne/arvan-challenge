615b0b23705a7246c1b17e715d0d706d4d9b404a
TheNerdOne <poria99chegeni@gmail.com>
2023-11-05 00:51:48 +0330

    feat:implement vue-router in app

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from './routes'
+import App from '../src/App.vue'
 import './style.css'
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'; import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axiosApiWrapper from './service/axiosApiWrapper'
 
-createApp(App).mount('#app')
+axiosApiWrapper.init()
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+const app = createApp(App)
+app.use(router)
+
+app.mount('#app')
