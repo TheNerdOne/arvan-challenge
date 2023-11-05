a22f1bf7500e077f979008daf76f6676eb62d482
TheNerdOne <poria99chegeni@gmail.com>
2023-11-05 13:11:38 +0330

    feat:use components in layout

---
+++ src/layout/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <TopBar>
+      <SideBar>
+        <slot />
+      </SideBar>
+    </TopBar>
+  </div>
+</template>
+
+<script setup>
+import SideBar from "../components/SideBar.vue";
+import TopBar from "../components/TopBar.vue";
+</script>
+
+<style lang="scss" scoped>
+::v-deep .content-slot > div {
+  display: flex;
+}
+</style>
