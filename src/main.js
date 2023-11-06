f3d6599a63dcc1e4cd0b9407324c3578281345d5
TheNerdOne <poria99chegeni@gmail.com>
2023-11-06 12:08:04 +0330

    feat:implement pinia as state management

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 import App from '../src/App.vue'
@@ -6,6 +7,8 @@ import './style.css'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axiosApiWrapper from './service/axiosApiWrapper'
+import { cookieFuns } from './service/cookies'
+const CookiesFunctions = new cookieFuns()
 
 axiosApiWrapper.init()
 
@@ -13,7 +16,11 @@ const router = createRouter({
     history: createWebHistory(),
     routes,
 })
+
 const app = createApp(App)
+const pinia = createPinia()
+
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
