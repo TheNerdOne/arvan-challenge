b3c3bade4fa70bb0865366b2401da51a6082027a
TheNerdOne <poria99chegeni@gmail.com>
2023-11-06 12:09:43 +0330

    feat:add name gor auth route

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import ArticleForm from './components/ArticleForm.vue'
 import Layout from './layout/index.vue'
 
 const routes = [
-    { path: '/auth', component: Auth },
+    { path: '/auth', name:"auth", component: Auth },
     { path: '/articles', name:"articles", meta: { layout: Layout }, component: Articles },
     { path: '/articles/create', name:"createArticle", meta: { layout: Layout }, component: ArticleForm },
     { path: '/articles/edit/:slug', name:"editArticle", meta: { layout: Layout }, component: ArticleForm },
