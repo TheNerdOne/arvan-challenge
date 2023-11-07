0671f8bbed279ce92b0d6c2b643c5832a528e89a
TheNerdOne <poria99chegeni@gmail.com>
2023-11-08 00:35:05 +0330

    feat:add delete action

--- src/stores/articles.js
+++ src/stores/articles.js
@@ -12,7 +12,15 @@ export const useArticlesStore = defineStore('articles', {
                 this.articlesData = res.data
             })
         },
+        async deleteArticle(slug) {
+            await articlesDataProvider.deleteArticle(slug).then((res) => {
+                let tempArticles = { ...this.articlesData }
+                tempArticles.articles = tempArticles.articles.filter((item) => { item.slug !== slug })
+                tempArticles.articlesCount--
+                this.articlesData = tempArticles
+            }).catch((e) => { console.log(e) });
 
+        },
         async createArticle(payload) {
             await articlesDataProvider.createArticle(payload).then((res) => {
                 let tempArticles = { ...this.articlesData }
