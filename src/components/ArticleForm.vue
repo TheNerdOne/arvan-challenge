3667311f823eac277073d68a6bdc5959e3a240f0
TheNerdOne <poria99chegeni@gmail.com>
2023-11-08 00:29:49 +0330

    feat: data binding & create article in store

--- src/components/ArticleForm.vue
+++ src/components/ArticleForm.vue
@@ -1,8 +1,8 @@
 <template>
-  <form class="form-container p-5" @submit="handleSubmit($event)">
+  <form class="form-container p-5" @submit.prevent="handleSubmit()">
     <div class="row mb-3">
       <div class="col-12">
-        <h1 class="text-center authHeader">
+        <h1 class="text-left authHeader">
           {{ editMode ? "Edit" : "New" }} Article
         </h1>
       </div>
@@ -10,23 +10,25 @@
     <div class="row mb-3">
       <div class="col-lg-9 col-md-6 col-sm-6 col-xs-12">
         <label for="user" class="col-12 col-form-label">Title</label>
-        <div class="col-12">
+        <div class="col-12 mb-3">
           <input
             type="text"
             required
             class="form-control"
             id="title"
+            placeholder="Title"
             v-model="title"
           />
         </div>
         <label for="description" class="col-12 col-form-label"
           >Description</label
         >
-        <div class="col-12">
+        <div class="col-12 mb-3">
           <input
             type="description"
             class="form-control"
             id="description"
+            placeholder="Description"
             v-model="description"
           />
         </div>
@@ -47,9 +49,9 @@
       <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
         <label for="user" class="col-12 col-form-label">Tags</label>
         <div class="col-12">
-          <div class="d-flex align-items-center justify-content-start gap-2">
-              <input type="checkbox" required id="title" v-model="title" />
-              <div>tag</div>
+          <div v-for="(tag,idx) in tags" class="d-flex align-items-center justify-content-start gap-2">
+              <input type="checkbox" :id="`tag${idx}`" @input="tagListHandler(tag,$event)"/>
+              <div>{{tag}}</div>
           </div>
         </div>
       </div>
@@ -59,17 +61,31 @@
 
 <script setup>
 import { computed, ref } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
+import { useArticlesStore } from "../stores/articles";
 const route = useRoute();
+const router = useRouter()
+const articlesStore = useArticlesStore()
 const editMode = computed(() => {
   return route.params.slug;
 });
 const title = ref("");
 const description = ref("");
 const body = ref("");
-const handleSubmit = (payload) => {
-  console.log(payload);
+const tagList = ref([])
+const tags = ref(["1","2","3"])
+const handleSubmit = async () => {
+  const payload = {title:title.value,description:description.value,body:body.value,tagList:tagList.value}
+  editMode ? await articlesStore.createArticle({article:payload}) : await editArticle({article:payload})
+  router.push({name:'articles'})
 };
+function tagListHandler(tag,e){
+  if(e.target.checked) {
+    tagList.value.push(tag)
+  } else {
+    tagList.value = tagList.value.filter((t)=>t!==tag)
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
