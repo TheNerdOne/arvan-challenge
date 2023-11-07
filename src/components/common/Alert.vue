4e88e40a3b5ce5919a5615f1bbe0ee106a111380
TheNerdOne <poria99chegeni@gmail.com>
2023-11-07 11:29:18 +0330

    feat:implement and use alert component

---
+++ src/components/common/Alert.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    :class="`alert alert-${config?.type} alert-dismissible fade ${visibility ? 'visibility' : ''}`"
+    role="alert"
+  >
+    <strong>{{ config?.strongText }}</strong
+    >{{ config?.text }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps(["config","visibility"]);
+</script>
+
+<style lang="scss" scoped>
+.alert {
+  position: absolute;
+  right: 30px;
+  top: 80px;
+}
+</style>
